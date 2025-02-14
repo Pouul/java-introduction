f3214a741dd2274d52cb1673b98ec1f4e3e5bd6f
Pouul <paulohuang04@gmail.com>
2025-02-14 11:56:04 +0000

    ex1

---
+++ App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println(maior (10,34,876));
+    }
+    public static void main(int a,int b,int c) {
+        //if (a>b && a>c) {return a};
+        //else if(b>c) return b;
+        //return c;
+    }
+}
