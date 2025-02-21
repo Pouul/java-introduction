8dafb64cffa5d2fd97fadb4d3367955a3573c150
Pouul <paulohuang04@gmail.com>
2025-02-21 00:23:15 +0000

    ex2

--- App.java
+++ App.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class Main {
+public class App {
  public static void main(String[] args) {
  Scanner scanner = new Scanner(System.in);
  System.out.print("Digite o numero: ");
- int numero = scanner.nextLine();
- system.out.println("os numeros serão");
- for(int i=0 ; i<=numero ; i++){
-    system.out.println(i);
+ int numero = scanner.nextInt();
+ System.out.println("a tabuada fica: ");
+ for(int i=1 ; i<=10 ; i++){
+   System.out.println(numero + " x " + i + " = " + numero*i);
+   
  }
  scanner.close();
  }
-}
+} 
